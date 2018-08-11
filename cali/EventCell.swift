82470f4f88323207bdd141a2a758f385ef55d00c
will3 <will3.git@gmail.com>
2018-08-11 17:38:03 +1200

    Fix constraint

--- cali/EventCell.swift
+++ cali/EventCell.swift
@@ -13,20 +13,28 @@ import Layouts
 class EventCell : UITableViewCell {
     static let identifier = "EventCell"
     
+    /// View did load
     private var didLoad = false
-    
+    /// Time label
     private let timeLabel = UILabel()
+    /// Duration label
     private let durationLabel = UILabel()
+    /// Title label
     private let titleLabel = UILabel()
+    /// Left
     private let left = UIView()
+    /// Right
     private let right = UIView()
+    /// Left width
+    let leftWidth : Float = 100.0
 
+    /// Event
     var event: Event? {
         didSet {
             updateEvent()
         }
     }
-
+    
     override func didMoveToSuperview() {
         super.didMoveToSuperview()
         
@@ -39,20 +47,24 @@ class EventCell : UITableViewCell {
     }
     
     private func loadView() {
-        titleLabel.numberOfLines = 0
-        layout(contentView).stackHorizontal([
+        titleLabel.numberOfLines = 4
+        
+        layout(contentView)
+            .translatesAutoresizingMaskIntoConstraints()
+            .stackHorizontal([
             layout(left)
+                .width(leftWidth)
                 .hugMore()
                 .resistMore()
                 .stack([
-                layout(timeLabel).left(18).top(14).hugMore(),
-                layout(durationLabel).left(18).top(2).bottom(18).hugMore()
-                ]),
+                    layout(timeLabel).left(18).top(14).hugMore(),
+                    layout(durationLabel).left(18).top(2).bottom(18).hugMore()
+                    ]),
             layout(right)
-                .stack([
-                    layout(titleLabel).left(18).right(18).top(12).hug(1).resist(1)
-                    ])
                 .justifyItems(.leading)
+                .stack([
+                     layout(titleLabel).left(18).top(12).right(18)
+                    ])
             ]).install()
         
         timeLabel.font = Fonts.fontSmall
